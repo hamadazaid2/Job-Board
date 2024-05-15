22d9d943cb86e99d48bcdbf65d67a579bf017f75
Hamada Zaid <82973458+hamadazaid2@users.noreply.github.com>
2024-05-15 10:16:12 +0300

    Enhancment | Move the query from controller to Model scope function

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -10,32 +10,49 @@ class JobController extends Controller
     /**
      * Display a listing of the resource.
      */
+
     public function index()
     {
-        $jobs = Job::query();
+        $filters = request()->only(
+            'search',
+            'min_salary',
+            'max_salary',
+            'experience',
+            'category'
+        );
 
-        // when => used to start execute the function when search sent in the request
-        $jobs->when(request('search'), function ($query) {
-            // this where is for set title or description inside ()
-            // select * from `jobs` where (`title` LIKE '%claim%' or `description` LIKE '%claim%') and `salary` <= '100000' and `salary` >= '60000'
-            $query->where(function ($query) {
-                $query->where('title', 'LIKE', '%' . request('search') . '%')
-                    ->orWhere('description', 'LIKE', '%' . request('search') . '%');
-            });
-        })->when(request('max_salary'), function ($query) {
-            $query->where('salary', '<=', request('max_salary'));
-        })->when(request('min_salary'), function ($query) {
-            $query->where('salary', '>=', request('min_salary'));
-        })->when(request('experience'), function ($query) {
-            $query->where('experience', request('experience'));
-        })->when(request('category'), function ($query) {
-            $query->where('category', request('category'));
-        });
-        return view("job.index", ['jobs' => $jobs->get()]);
+        return view('job.index', ['jobs' => Job::filter($filters)->get()]);
     }
 
+    // public function index()
+    // {
+    //     $jobs = Job::query();
+
+    //     // when => used to start execute the function when search sent in the request
+    //     $jobs->when(request('search'), function ($query) {
+    //         // this where is for set title or description inside ()
+    //         // select * from `jobs` where (`title` LIKE '%claim%' or `description` LIKE '%claim%') and `salary` <= '100000' and `salary` >= '60000'
+    //         $query->where(function ($query) {
+    //             $query->where('title', 'LIKE', '%' . request('search') . '%')
+    //                 ->orWhere('description', 'LIKE', '%' . request('search') . '%');
+    //         });
+    //     })->when(request('max_salary'), function ($query) {
+    //         $query->where('salary', '<=', request('max_salary'));
+    //     })->when(request('min_salary'), function ($query) {
+    //         $query->where('salary', '>=', request('min_salary'));
+    //     })->when(request('experience'), function ($query) {
+    //         $query->where('experience', request('experience'));
+    //     })->when(request('category'), function ($query) {
+    //         $query->where('category', request('category'));
+    //     });
+    //     return view("job.index", ['jobs' => $jobs->get()]);
+    // }
+
     /**
-     * Show the form for creating a new resource.
+     
+
+    
+    * Show the form for creating a new resource.
      */
     public function create()
     {
